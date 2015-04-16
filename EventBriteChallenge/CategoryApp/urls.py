3ba9ff22ea2c0240cf40033a4cc42fc966f102de
Dominic Bett <dominicbett@hotmail.com>
2015-04-16 17:14:58 -0600

    Working on template

--- EventBriteChallenge/CategoryApp/urls.py
+++ EventBriteChallenge/CategoryApp/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import ListView, DetailView
-from CategoryApp.models import Category
+#from CategoryApp.models import Category
+from CategoryApp.challenge import Challenge
 
 urlpatterns = patterns('',
 						url(r'^$', ListView.as_view(
-								queryset=Category.objects.all().order_by("-date")[:10],
+								queryset=Challenge.event,
 								template_name="index.html")),
 						)
